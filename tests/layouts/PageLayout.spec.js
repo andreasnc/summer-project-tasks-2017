1838a1ae32a5c59bb4e484bd53afc937687a2f6f
fredrickb <fredrickb@netcompany.com>
2017-06-20 21:06:43 +0200

    Removed test-references to old components and updated existing according to new setup

--- tests/layouts/PageLayout.spec.js
+++ tests/layouts/PageLayout.spec.js
@@ -8,7 +8,7 @@ describe('(Layout) PageLayout', () => {
   })
 
   it('renders a project title', () => {
-    shallow(<PageLayout />).find('h1').should.have.text('React Redux Starter Kit')
+    shallow(<PageLayout />).find('a').should.have.text('Login')
   })
 
   it('renders its children inside of the viewport', () => {
